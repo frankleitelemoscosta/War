82529a4921c3600eb7dc08671d697f327455ce51
frankleitelemoscosta <frankleite090@gmail.com>
2023-01-02 11:56:59 -0300

    Updating the Readme

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ foi lançado em 1957 com o nome de la conquête du monde("A Conquista do Mundo")
 como dito por Lamorisse o jogo simula a conquista de paises por um jogador, tendo
 como vencedor aquele que cumprir os objetivos descritos nas cartas primeiro
 tendo conquista o mundo.
+
+## DECISÕES DE IMPLEMENTAÇÃO E RESULTADOS
+
+### A PRIMEIRA DECISÃO
+
+A primeira decisão feita foi a de começar um diagrama para que pudessemos ter 
+noção de quais rumos tomar com o projeto.
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,46 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include "/home/frank/Documentos/PROJETOS FORA DA FACUL/WAR/hpp/paises.hpp"
+//bibliotecas
+    #include <iostream>
+    #include <string>
+    #include <vector>
+    #include "/home/frank/Documentos/PROJETOS FORA DA FACUL/WAR/hpp/paises.hpp"
+//fim das bibliotecas
+
 using namespace std;
 
 int main()
 {
+
+    //variaveis locais
+        int numero_de_jogadores,   error;
+        string *vetor;
+    //fim das variaveis locais
     
+    cout<<"Quantos serão os jogadores?"<<endl;
+    
+    //tratamento de excessão do imput do numero de jogadores
+        do{
+            error = 0;
+            cin>>numero_de_jogadores;
+            if(cin.fail())
+            {
+                error = 1;
+                cout<<"Digite um inteiro válido"<<endl;
+                cin.clear();
+                cin.ignore();
+            }
+        }while(error == 1);
+    //fim
+
+    vetor = new string[numero_de_jogadores];
+
+
+    for(int i = 0 ; i < numero_de_jogadores ; i++)
+    {
+        cin.clear();
+        cin.ignore();
+        cout<<"Qual o nome do jogador "<<i + 1<<" ?"<<endl;
+        getline(cin,vetor[i]);
+    }
+
     return 0;
 }
